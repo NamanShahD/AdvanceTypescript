3e836142300e4c6e0c3c286bf2e0d3966c4276d2
NamanShahD <162907281+NamanShahD@users.noreply.github.com>
2024-04-20 21:53:54 +0530

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function sumOfAge(user1, user2) {
+    return user1.age + user2.age;
+}
+sumOfAge({
+    name: 'Taro',
+    age: 54
+}, {
+    name: "Rato",
+    age: 57
+});
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+// interface User {
+//     name:string,
+//     age:number,
+// }
+// function sumOfAge(user1:User,user2:User){
+//     return user1.age + user2.age
+// }
+// sumOfAge({
+//     name:'Taro',
+//     age:54
+// },{
+//     name:"Rato",
+//     age:57
+// })
+// interface User{
+//     id:string,
+//     name:string,
+//     age:number,
+//     email:string,
+//     password:string
+// }
+// //Here i just want to update name,age,email
+// type UpdateProps = Pick<User,'name'|'age'|'email'>
+// function updateUser(updateProps:UpdateProps){
+
+// }
