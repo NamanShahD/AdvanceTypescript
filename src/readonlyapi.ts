d9e1813e38deca2e1e378fbd229e2edf85c27e4d
NamanShahD <162907281+NamanShahD@users.noreply.github.com>
2024-04-21 13:50:58 +0530

    Adding record and Map

--- src/readonlyapi.ts
+++ src/readonlyapi.ts
@@ -1,8 +1,14 @@
-type User = {
-     age:number,
-    name:string
-}
-const user :Readonly <User> ={
-    age:21,
-    name:"Rajesh"
+// type User = {
+//      age:number,
+//     name:string
+// }
+// const user :Readonly <User> ={
+//     age:21,
+//     name:"Rajesh"
+// }
+//Record And Map
+type User = Record<string,{age:number,name:string}> //Record <string,User>
+const user :User = {
+    "raaq@123":{age:21,name:"harish"},
+    "raaaq@123": {age:42 , name : "bsnda"}
 }
