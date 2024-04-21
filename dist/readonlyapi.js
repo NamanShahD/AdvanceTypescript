c6ebbf01d1a39de5cdb2273af248b7b408dc9122
NamanShahD <162907281+NamanShahD@users.noreply.github.com>
2024-04-21 11:15:57 +0530

    Adding readonly file

---
+++ dist/readonlyapi.js
@@ -0,0 +1,5 @@
+"use strict";
+const user = {
+    age: 21,
+    name: "Rajesh"
+};
---
+++ src/readonlyapi.ts
@@ -0,0 +1,8 @@
+type User = {
+     age:number,
+    name:string
+}
+const user :Readonly <User> ={
+    age:21,
+    name:"Rajesh"
+}
